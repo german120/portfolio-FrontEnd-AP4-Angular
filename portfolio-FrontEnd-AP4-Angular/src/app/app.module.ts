7432c9c72bda5f0953b9d96e2531619cf7a6d850
german120 <germanblasco120@gmail.com>
2023-02-13 18:31:50 -0300

    Migrado todo a componentes

--- portfolio-FrontEnd-AP4-Angular/src/app/app.module.ts
+++ portfolio-FrontEnd-AP4-Angular/src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EncabezadoComponent } from './componentes/encabezado/encabezado.component';
+import { BannerComponent } from './componentes/banner/banner.component';
+import { PresentacionComponent } from './componentes/presentacion/presentacion.component';
+import { ExperienciaComponent } from './componentes/experiencia/experiencia.component';
+import { EducacionComponent } from './componentes/educacion/educacion.component';
+import { SkillsComponent } from './componentes/skills/skills.component';
+import { VentanaModalComponent } from './componentes/ventana-modal/ventana-modal.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EncabezadoComponent,
+    BannerComponent,
+    PresentacionComponent,
+    ExperienciaComponent,
+    EducacionComponent,
+    SkillsComponent,
+    VentanaModalComponent
   ],
   imports: [
     BrowserModule,
